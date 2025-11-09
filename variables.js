162ab8432ac9388e2c4a7e34292f74300ba4d0a0
Martin Bram Moreinis <martin.moreinis@schoolsofwestfield.org>
2025-11-09 16:00:32 -0500

    Create variables.js for variable exercises
    Added a JavaScript file with variable naming and assignment exercises, including alerts and prompts.

---
+++ variables.js
@@ -0,0 +1,80 @@
+
+/* Animals and legs:
+ * Practicing variable naming and assignment, including camelCase.
+ */
+
+// Create (declare) a variable and name it legs
+
+// Assign 4 to that variable. It is holding an integer (number)
+
+// Create (declare) a variable and name it animalType
+
+// Assign the name of a type of animal with the current value of legs to animalType. It is holding a string (symbols)
+
+// Display an Alert Box with the message: "An [animalType] has [legs] legs"
+
+// Subtract 2 from legs and assign that to legs (not in this order)
+
+// Assign the name of a type of animal with the new value of legs to animalType
+
+// Display an Alert Box with the message: "An [animalType] has [legs] legs"
+
+// Multiply the current value of legs by 3 and assign that to legs
+
+// Assign the name of a type of animal with the new value of legs to animalType
+
+// Display an Alert Box with the message: "An [animalType] has [legs] legs"
+
+// Add 2 to the current value of legs and assign that to legs
+
+// Assign the name of a type of animal with the new value of legs to animalType
+
+// Display an Alert Box with the message: "An [animalType] has [legs] legs"
+
+// Multiply the current value of legs by 4 and assign that to legs
+
+// Assign the name of a type of animal which could have the new value of legs to animalType
+
+// Display an Alert Box with the message: "An [animalType] SOMETIMES has [legs] legs"
+
+// Assign the value of 3 to legs
+
+// Assign the string "amputee" to animalType
+
+// Display an Alert Box with the message: "An [animalType] SOMETIMES has [legs] legs"
+
+// Assign the string "dog" to animalType
+
+// Display an Alert Box with the message: "for example, an [animalType]".
+
+/* Fun with expressions 
+ * Understanding the pecularities of the JavaScript Alert Box
+ */
+
+// Declare a variable and name it first
+
+// Assign a value to first using a prompt: "Enter first number"
+
+// Declare a variable and name it second
+
+// Assign a value to second using a prompt: "Enter second number"
+
+// Declare a variable and name it bragging
+
+// Assign exactly this expression to bragging:  "I can add " + first + " + " + second"
+
+// Display an Alert Box with bragging inside it. 
+
+// Assign exactly this expression to bragging: first + " + " + second + " = " + first + second
+
+// DO NOT Display an Alert Box with bragging inside it YET. 
+
+// Declare a variable and name it prediction
+
+// Assign your prediction about what will be displayed when we display bragging
+
+// Display an Alert Box with prediction inside it. 
+
+// Call Mr. M. over to see it.  Do not do anythign else!
+
+// Await further instructions. 
